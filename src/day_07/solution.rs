ffa8aef47c97b578ca110f8a6906a520e6c18df0
s-braitsch <simon.braitsch@novatec-gbmh.de>
2023-12-08 09:20:34 +0100

    day 7 refactor

--- src/day_07/solution.rs
+++ src/day_07/solution.rs
@@ -8,18 +8,19 @@ struct Hand {
     all: String,
     value: usize,
     bid: usize,
-    part: usize,
+    allow_jokers: bool,
 }
 
-static PART: usize = 2;
-
-fn char_to_num(c: char, part: usize) -> u32 {
+fn char_to_num(c: char, allow_jokers: bool) -> u32 {
     match c {
         'T' => 10,
-        'J' => match part {
-            2 => 1,
-            _ => 11,
-        },
+        'J' => {
+            if allow_jokers {
+                1
+            } else {
+                11
+            }
+        }
         'Q' => 12,
         'K' => 13,
         'A' => 14,
@@ -38,8 +39,8 @@ impl Ord for Hand {
         match self.value.cmp(&other.value) {
             Ordering::Equal => {
                 for i in 0..5 {
-                    let a = char_to_num(self.all.chars().nth(i).unwrap(), self.part);
-                    let b = char_to_num(other.all.chars().nth(i).unwrap(), self.part);
+                    let a = char_to_num(self.all.chars().nth(i).unwrap(), self.allow_jokers);
+                    let b = char_to_num(other.all.chars().nth(i).unwrap(), self.allow_jokers);
                     return match a.cmp(&b) {
                         Ordering::Equal => continue,
                         order => order,
@@ -53,53 +54,53 @@ impl Ord for Hand {
 }
 
 impl Hand {
-    fn from_input(line: String, part: usize) -> Self {
+    fn from_input(line: String, allow_jokers: bool) -> Self {
         let (all, bid) = line.split_once(' ').unwrap();
         Hand {
             all: String::from(all),
-            value: Hand::get_hand_value(all, part),
+            value: Hand::get_hand_value(all, allow_jokers),
             bid: bid.parse::<usize>().unwrap(),
-            part,
+            allow_jokers,
         }
     }
 
-    fn parse_hand(cards: &str) -> (HashMap<char, usize>, usize) {
+    fn parse_hand(cards: &str, allow_jokers: bool) -> (Vec<usize>, usize) {
         let mut card_map: HashMap<char, usize> = HashMap::new();
         cards.chars().for_each(|c| {
             card_map.entry(c).and_modify(|v| *v += 1).or_insert(1);
         });
         let jokers = *card_map.get(&'J').unwrap_or(&0);
-        (card_map, jokers)
+        if allow_jokers && jokers > 0 {
+            let max_char = card_map
+                .iter()
+                .filter(|(k, _)| **k != 'J')
+                .max_by(|(_, a), (_, b)| a.cmp(b))
+                .unwrap_or((&'A', &0))
+                .0;
+            card_map
+                .entry(*max_char)
+                .and_modify(|v| *v += jokers)
+                .or_insert(5);
+            card_map.remove(&'J');
+        }
+        let values = card_map.into_values().collect::<Vec<usize>>();
+        let max = *values.iter().max().unwrap();
+        (values, max)
     }
 
-    fn get_hand_value(cards: &str, part: usize) -> usize {
-        let (card_map, jokers) = Hand::parse_hand(cards);
-        let values;
-        let max: usize;
-        if part == 1 {
-            values = card_map.into_values().collect::<Vec<usize>>();
-            max = *values.iter().max().unwrap();
-        } else {
-            values = card_map
-                .into_iter()
-                .filter(|(k, _)| *k != 'J')
-                .map(|(_, v)| v)
-                .collect();
-            max = values.iter().max().unwrap_or(&0) + jokers;
-        };
+    fn get_hand_value(cards: &str, allow_jokers: bool) -> usize {
+        let (values, max) = Hand::parse_hand(cards, allow_jokers);
 
         match max {
             x if x > 3 => x + 1,
             3 => {
-                return match part {
-                    1 => if values.contains(&2) { 4 } else { 3 },
-                    _ => if (values.iter().filter(|v| **v == 2).count() == 2)
-                        || (jokers == 0 && values.contains(&2)) {
-                        4
-                    } else { 3 }
+                if values.contains(&2) {
+                    4
+                } else {
+                    3
                 }
             }
-            2 => values.iter().filter(|v| **v == 2usize).count().max(1),
+            2 => values.iter().filter(|v| **v == 2usize).count(),
             _ => 0,
         }
     }
@@ -113,24 +114,18 @@ pub fn solve() {
 }
 
 fn part_one() -> usize {
-    sum_solution(1)
+    sum_solution(false)
 }
 
 fn part_two() -> usize {
-    sum_solution(2)
+    sum_solution(true)
 }
 
-fn sum_solution(part: usize) -> usize {
+fn sum_solution(allow_jokers: bool) -> usize {
     let mut hands = utils::file_to_lines("07")
         .into_iter()
-        .map(|line| Hand::from_input(line, part))
+        .map(|line| Hand::from_input(line, allow_jokers))
         .collect::<Vec<Hand>>();
     hands.sort();
-    hands
-        .iter()
-        .enumerate()
-        .map(|(i, h)| {
-            (i + 1) * h.bid
-        })
-        .sum()
+    hands.iter().enumerate().map(|(i, h)| (i + 1) * h.bid).sum()
 }
