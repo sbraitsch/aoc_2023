0e587f576a1ff3f22707eeaf48ff28dd37c63ccf
Simon Braitsch <S.Braitsch@gmx.de>
2023-12-09 13:08:59 +0100

    day 9, refactor

--- src/day_09/solution.rs
+++ src/day_09/solution.rs
@@ -20,36 +20,29 @@ pub fn solve() {
 }
 
 fn part_one(histories: &Vec<Vec<isize>>) -> isize {
-    histories.iter().map(|h| extrapolate_val(h, false)).sum()
+    histories.iter().cloned().map(|h| extrapolate(h)).sum()
 }
 
 fn part_two(histories: &Vec<Vec<isize>>) -> isize {
-    histories.iter().map(|h| extrapolate_val(h, true)).sum()
+    histories
+        .iter()
+        .cloned()
+        .map(|mut h| {
+            h.reverse();
+            extrapolate(h)
+        })
+        .sum()
 }
 
-fn extrapolate_val(history: &Vec<isize>, prev: bool) -> isize {
-    let mut current_history = history.clone();
-    let mut edge_values = Vec::new();
-    match prev {
-        true => edge_values.push(*history.first().unwrap()),
-        false => edge_values.push(*history.last().unwrap()),
-    }
+fn extrapolate(mut current_history: Vec<isize>) -> isize {
+    let mut edge_values = vec![*current_history.last().unwrap()];
     while current_history.iter().any(|v| *v != 0) {
         current_history = current_history
             .iter()
             .tuple_windows()
             .map(|(a, b)| b - a)
             .collect();
-        match prev {
-            true => edge_values.push(*current_history.first().unwrap()),
-            false => edge_values.push(*current_history.last().unwrap()),
-        }
+        edge_values.push(*current_history.last().unwrap());
     }
-    return match prev {
-        true => edge_values[..edge_values.len() - 1]
-            .iter()
-            .rev()
-            .fold(0, |acc, x| x - acc),
-        false => edge_values.iter().sum(),
-    };
+    edge_values.iter().sum()
 }
