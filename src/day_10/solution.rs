5080bd525d429b4a7724305a850ddb36016ab205
Simon Braitsch <S.Braitsch@gmx.de>
2023-12-10 22:53:58 +0100

    day 10, refactored, removed not working winding alg

--- src/day_10/solution.rs
+++ src/day_10/solution.rs
@@ -1,54 +1,41 @@
+use crate::day_10::solution::Direction::{DOWN, LEFT, RIGHT, UP};
 use crate::utils;
 use rayon::prelude::*;
-use std::collections::HashMap;
 use std::time::Instant;
+
+#[derive(Debug)]
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
 
 pub fn solve() {
     let lines = utils::file_to_lines("10");
     let mut start_idx = 0;
-
-    let mut pipes: HashMap<usize, Vec<usize>> = HashMap::new();
-    lines.iter().enumerate().for_each(|(y, line)| {
-        line.chars().enumerate().for_each(|(x, c)| {
-            let idx: isize = (y * line.len() + x) as isize;
-            let links = match c {
-                '|' => vec![idx - line.len() as isize, idx + line.len() as isize],
-                '-' => vec![idx - 1, idx + 1],
-                'L' => vec![idx - line.len() as isize, idx + 1],
-                'J' => vec![idx - line.len() as isize, idx - 1],
-                'F' => vec![idx + line.len() as isize, idx + 1],
-                '7' => vec![idx + line.len() as isize, idx - 1],
-                'S' => {
-                    start_idx = usize::try_from(idx).unwrap();
-                    let starts = vec![
-                        idx - line.len() as isize,
-                        idx + line.len() as isize,
-                        idx - 1,
-                        idx + 1,
-                    ];
-                    starts
-                }
-                _ => vec![],
-            };
-            pipes.insert(
-                y * line.len() + x,
-                links
-                    .iter()
-                    .filter(|i| {
-                        let range = 0..(lines.len() * line.len()) as isize;
-                        range.contains(i)
-                    })
-                    .map(|i| usize::try_from(*i).unwrap())
-                    .collect(),
-            );
+    let dim = lines[0].len();
+    let map: Vec<Vec<char>> = lines
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            if let Some(idx) = line.chars().position(|c| c == 'S') {
+                start_idx = y * line.len() + idx;
+            }
+            line.chars().collect()
         })
-    });
+        .collect();
 
     let mut time = Instant::now();
-    let start_points = &pipes[&start_idx];
+    let start_points = vec![
+        start_idx - 1,
+        start_idx + 1,
+        start_idx - dim,
+        start_idx + dim,
+    ];
     let max_loop = start_points
         .iter()
-        .map(|first| get_loop_indices(first, start_idx, &pipes))
+        .map(|first| get_loop(&map, start_idx, *first, dim))
         .max_by(|a, b| a.len().cmp(&&b.len()))
         .unwrap();
 
@@ -61,6 +48,73 @@ pub fn solve() {
     );
 }
 
+fn get_loop(map: &Vec<Vec<char>>, start: usize, first: usize, dim: usize) -> Vec<usize> {
+    let mut indices = vec![];
+    let mut cur = first;
+    let mut prev = start;
+    let mut next;
+    while cur != start {
+        let dir = get_dir(&cur, &prev);
+        next = match map[cur / dim][cur % dim] {
+            '|' => match dir {
+                UP => cur - dim,
+                DOWN => cur + dim,
+                _ => start,
+            },
+            '-' => match dir {
+                LEFT => cur - 1,
+                RIGHT => cur + 1,
+                _ => start,
+            },
+            'L' => match dir {
+                LEFT => cur - dim,
+                DOWN => cur + 1,
+                _ => start,
+            },
+            'J' => match dir {
+                RIGHT => cur - dim,
+                DOWN => cur - 1,
+                _ => start,
+            },
+            'F' => match dir {
+                LEFT => cur + dim,
+                UP => cur + 1,
+                _ => start,
+            },
+            '7' => match dir {
+                RIGHT => cur + dim,
+                UP => cur - 1,
+                _ => start,
+            },
+            'S' => start,
+            _ => start,
+        };
+        indices.push(cur);
+        prev = cur;
+        cur = next;
+    }
+    indices
+}
+
+fn get_dir(cur: &usize, prev: &usize) -> Direction {
+    match cur > prev {
+        true => {
+            if cur.abs_diff(*prev) == 1 {
+                RIGHT
+            } else {
+                DOWN
+            }
+        }
+        false => {
+            if cur.abs_diff(*prev) == 1 {
+                LEFT
+            } else {
+                UP
+            }
+        }
+    }
+}
+
 fn part_one(lp: &Vec<usize>) -> usize {
     lp.len() / 2 + 1
 }
@@ -68,61 +122,15 @@ fn part_one(lp: &Vec<usize>) -> usize {
 fn part_two(lp: &Vec<usize>, lines: Vec<String>) -> usize {
     let offset = lines.len() / 4;
     let range = offset..lines.len() - offset;
-    lines[range.clone()].iter().enumerate().map(|(y, line)| {
-        line[range.clone()].chars().enumerate().filter(|(x,char)| {
-            !lp.contains(&((y + offset) * line.len() + x + offset))
-        }).count()
-    }).sum()
-}
-
-
-fn get_loop_indices(first: &usize, start: usize, pipes: &HashMap<usize, Vec<usize>>) -> Vec<usize> {
-    let mut indices = vec![];
-    let mut cur = first;
-    let mut prev = start;
-    let mut next = &pipes[&cur]
+    lines[range.clone()]
         .iter()
-        .find(|idx| **idx != prev)
-        .unwrap_or(&start)
-        .clone();
-    while *next != start {
-        indices.push(*cur);
-        prev = *cur;
-        cur = next;
-        next = &pipes[cur]
-            .iter()
-            .find(|idx| **idx != prev)
-            .unwrap_or(&start);
-    }
-    indices
-}
-
-fn winding_num(indices: &Vec<usize>, cols: usize, point: (usize, usize)) -> bool {
-    let mut winding_number = 0;
-    let (x, y) = point;
-
-    for i in 0..indices.len() {
-        let current_index = indices[i];
-        let next_index = indices[(i + 1) % indices.len()];
-
-        let current_row = current_index / cols;
-        let current_col = current_index % cols;
-
-        let next_row = next_index / cols;
-        let next_col = next_index % cols;
-
-        if (current_row <= y && y < next_row) || (next_row <= y && y < current_row) {
-            let cross_product = (next_col as isize - current_col as isize)
-                * (y as isize - current_row as isize)
-                - (next_row as isize - current_row as isize) * (x as isize - current_col as isize);
-
-            if cross_product > 0 {
-                winding_number += 1;
-            } else if cross_product < 0 {
-                winding_number -= 1;
-            }
-        }
-    }
-
-    winding_number != 0
+        .enumerate()
+        .map(|(y, line)| {
+            line[range.clone()]
+                .chars()
+                .enumerate()
+                .filter(|(x, char)| !lp.contains(&((y + offset) * line.len() + x + offset)))
+                .count()
+        })
+        .sum()
 }
